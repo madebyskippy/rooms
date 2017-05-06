7ab4bfffc6b74a04dc6d21be8848319f0775af72
madebyskippy <timothys@nyu.edu>
2017-05-06 19:33:00 -0400

    moving from 0 to 1 and IT LOOKS COOL but the rest of the logic isnt there yet

--- Assets/linemanager.cs
+++ Assets/linemanager.cs
@@ -4,13 +4,39 @@ using UnityEngine;
 
 public class linemanager : MonoBehaviour {
 
+	[SerializeField] lineobject[] stuffs;
+
+	bool move;
+	int direction;
+
 
 	// Use this for initialization
 	void Start () {
+		move = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			move = true;
+			direction = 1;
+		}if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			move = true;
+			direction = -1;
+		}if (Input.GetKeyUp (KeyCode.RightArrow)) {
+			move = false;
+		}if (Input.GetKeyUp (KeyCode.LeftArrow)) {
+			move = false;
+		}
+
+		if (move) {
+			step(direction);
+		}
+	}
+
+	void step(int direction){
+		for (int i = 0; i < stuffs.Length; i++) {
+			stuffs [i].step (direction);
+		}
 	}
 }
--- Assets/lineobject.cs
+++ Assets/lineobject.cs
@@ -4,20 +4,68 @@ using UnityEngine;
 
 public class lineobject : MonoBehaviour {
 
-	[SerializeField] Mesh mesh;
+	[SerializeField] Mesh[] mesh = new Mesh[2];
+
+	int duration = 100; //how long it takes to switch, in steps
+	int index;
+	int current; //which mesh you're on
+	int target; //which one you're moving to
 
 	LineRenderer line;
+
+	List<Vector3[]> views;
+
+	Vector3[] verts;
 
 	// Use this for initialization
 	void Start () {
 		line = GetComponent<LineRenderer> ();
-		line.positionCount = mesh.vertices.Length+1;
-		line.SetPositions (mesh.vertices);
-		line.SetPosition (mesh.vertices.Length, mesh.vertices [0]);
+		views = new List<Vector3[]> ();
+		for (int i=0; i<mesh.Length; i++){
+			Vector3[] view = new Vector3[mesh[0].vertices.Length + 1];
+			if (i==0)
+				verts = new Vector3[mesh[0].vertices.Length + 1];
+			for (int j = 0; j < view.Length-1; j++) {
+				if (i==0)
+					verts [j] = mesh[i].vertices [j];
+				view [j] = mesh[i].vertices [j];
+			}
+			if (i==0)
+				verts [verts.Length - 1] = mesh[i].vertices [0];
+			view [view.Length - 1] = mesh[i].vertices [0];
+			views.Add (view);
+		}
+		line.positionCount = verts.Length;
+		line.SetPositions (views[0]);
+
+		current = 0;
+		index = 0;
+		target = 1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public void step(int direction){
+		//figure out what to change the points to
+		index += direction;
+
+		Debug.Log (index);
+		if (index < 0) {
+			index = 0;
+		} if (index > duration) {
+			index = 0;
+			current = target;
+		}
+
+
+		//actually change the points
+		for (int i = 0; i < verts.Length; i++) {
+			//lerp between current and the next one ..... given index/duration
+			verts[i] = Vector3.Lerp(views[current][i], views[target][i], (float)index/(float)duration);
+		}
+		line.SetPositions (verts);
 	}
 }
